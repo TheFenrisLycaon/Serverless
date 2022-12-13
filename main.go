7852a8e4421266a1a08524c7fe344e5950c1ed41
Fenris Lycaon <thefenrislycaon@gmail.com>
2022-12-13 18:33:19 +0530

    Fix Imports

--- main.go
+++ main.go
@@ -2,18 +2,24 @@ package main
 
 import (
 	"os"
+
 	"github.com/TheFenrisLycaon/Serverless/pkg/handlers"
-	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var (
+	dynaClient dynamodbiface.DynamoDBAPI
 )
 
 func main() {
 	region := os.Getenv("AWS_REGION")
 	aws_session, err := session.NewSession(&aws.Config{
-		Region : aws.String(region),
+		Region: aws.String(region),
 	})
 
 	if err != nil {
@@ -26,8 +32,8 @@ func main() {
 
 const tableName = "LambdaInGoUser"
 
-func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error){
-	switch req.HTTPMethod{
+func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	switch req.HTTPMethod {
 	case "GET":
 		return handlers.GetUser(req, tableName, dynaClient)
 	case "POST":
